8c78e40aa2df523ad6e10b7ee74df03f12d85b46
Viacheslav Borisov <borisov.slava00@yandex.ru>
2024-08-11 12:12:59 +0400

    push on server

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,15 +5,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
-
 SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
 
 DEBUG = os.environ.get('DEBUG', default=False)
 
-#ALLOWED_HOSTS = ['158.160.94.168', '127.0.0.1', 'localhost', 'kittygram123.hopto.org']
-
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
